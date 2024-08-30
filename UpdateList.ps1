f548ccc508d8abdbf79b4e44a62c3b64cc63961e
SERP-debug <148842204+SERP-debug@users.noreply.github.com>
2024-08-30 11:35:06 +0300

    результат

--- UpdateList.ps1
+++ UpdateList.ps1
@@ -1,62 +1,38 @@
-﻿function Get-IniFile 
-{  
-    param(  
-        [parameter(Mandatory = $true)] [string] $filePath  
-    )  
-    
-    $anonymous = "NoSection"
-  
-    $ini = @{}  
-    switch -regex -file $filePath  
-    {  
-        "^\[(.+)\]$" # Section  
-        {  
-            $section = $matches[1]  
-            $ini[$section] = @{}  
-            $CommentCount = 0  
-        }  
+﻿[CmdletBinding()]
 
-        "^(;.*)$" # Comment  
-        {  
-            if (!($section))  
-            {  
-                $section = $anonymous  
-                $ini[$section] = @{}  
-            }  
-            $value = $matches[1]  
-            $CommentCount = $CommentCount + 1  
-            $name = "Comment" + $CommentCount  
-            $ini[$section][$name] = $value  
-        }   
+Param (
+[Parameter (Mandatory=$true, Position=1)]
+[string]$PathIniFile
+)
 
-        "(.+?)\s*=\s*(.*)" # Key  
-        {  
-            if (!($section))  
-            {  
-                $section = $anonymous  
-                $ini[$section] = @{}  
-            }  
-            $name,$value = $matches[1..2]  
-            $ini[$section][$name] = $value  
-        }  
-    }  
+#Получение параметров из config.ini
+Get-Content $PathIniFile | foreach-object -begin {$h=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $h.Add($k[0], $k[1]) } }
 
-    return $ini  
-}  
-$SettingsIni = Get-IniFile "C:\_PowerShell\List of Catologists in the clipboard\configfile.ini"
+$DirREPOSITORY = $h.Get_Item("DirREPOSITORY")
+$TmpHTML       = $h.Get_Item("TmpHTML")
+$ResultHTML    = $h.Get_Item("HTML")
 
-$DirREPOSITORY = $SettingsIni.database.DirREPOSITORY
-$TmpHTML       = $SettingsIni.database.TmpHTML
-$HTML          = $SettingsIni.database.HTML
-
+# Загрузка и сортировка папок ИБ 
 $bsort = Get-Childitem -Path $DirREPOSITORY -Directory | Sort-Object -Property LastWriteTime -Descending
- 
-$string = ""
-$bsort | ForEach-Object {$string += "<div>`n"+
-"<input type=""text"" value="""+"$_"+""" readonly>`n"+
-"<input type=""text"" class=""SERP"" id="""+$_+""" value= ""tcp://vcs/"+"$_"+""" readonly>`n"+""+
-"<button onclick=""copy('"+"$_"+"')""><svg xmlns=""http://www.w3.org/2000/svg"" width=""12"" height=""12"" viewBox=""0 0 24 24"" fill=""none"" stroke=""currentColor"" stroke-width=""2"" stroke-linecap=""round"" stroke-linejoin=""round"" class=""feather feather-copy""><rect x=""9"" y=""9"" width=""13"" height=""13"" rx=""2"" ry=""2""></rect><path d=""M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1""></path></svg></button>`n"+
-"<div>`n"}
 
+# Полечение текста замены
+$ReplaceText = ApplyTmp
+# Получение шаблона
 $Content = Get-Content $TmpHTML
-$Content.replace(‘<div>anchor</div>’,$string)| Out-File -Encoding utf8 $HTML
+# Подставка текста замены в шаблон и запись в итоговый файл
+$Content.replace(‘<div>anchor</div>’,$ReplaceText)| Out-File -Encoding utf8 $ResultHTML
+
+function ApplyTmp
+{
+#Применение шаблона к считанному массиву ИБ 
+$ReplaceableText = ""
+$bsort | ForEach-Object{
+$ReplaceableText += 
+    "<div>`n"+
+        "<input type=""text"" value="""+"$_"+""" readonly>`n"+
+        "<input type=""text"" class=""SERP"" id="""+$_+""" value= ""tcp://vcs/"+"$_"+""" readonly>`n"+""+
+        "<button onclick=""copy('"+"$_"+"')""><svg xmlns=""http://www.w3.org/2000/svg"" width=""12"" height=""12"" viewBox=""0 0 24 24"" fill=""none"" stroke=""currentColor"" stroke-width=""2"" stroke-linecap=""round"" stroke-linejoin=""round"" class=""feather feather-copy""><rect x=""9"" y=""9"" width=""13"" height=""13"" rx=""2"" ry=""2""></rect><path d=""M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1""></path></svg></button>`n"+
+    "<div>`n"
+}
+    return $ReplaceableText
+}
